65f1e8a895851aa9b2c7936fbc8b3ba3c87898f9
DanielAzhar <68184218+DanielAzhar@users.noreply.github.com>
2020-07-12 05:16:50 -0700

    Add files via upload

---
+++ bot/botConfig.php
@@ -0,0 +1,22 @@
+<?php
+//Emoji Code 
+//To Get Emoji Code
+//For Android ( :- + Emoji )
+//For PC ( / + Emoji )
+
+$stars = "<:starr:731802975051448321>";
+$CP = "<:cp:731802213659443280>";
+$goldCoins = "<:coin:731801884247064609>";
+$coins = "<:usercoin:731804087258972250>";
+$unverifycoin = "<:user_coin_unverified:731842144385368074>";
+$demon = "<:demon:731802313970155560>";
+$like = "<:like:731802825255813150>";
+$YT = "<:yt:731804287713411134>";
+$twitter = "<:twitter:731803994099286098>";
+$twitch = "<:twitch:731803923605749761>";
+$play = "<:play:731843610491879434>";
+$download = "<:downloads:731802653797122158>";
+$lengthlvl = "<:length:731843645048881203>";
+
+//For See Diff Level Its Already set In Bot Files
+?>
